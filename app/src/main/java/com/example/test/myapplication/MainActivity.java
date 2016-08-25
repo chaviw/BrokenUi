fb18871ce1140c99594f9232cbb9f60fd2035c7c
Chavi Weingarten <chavi@pearlauto.com>
2016-08-24 18:38:12 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/test/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.test.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private View image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        image = findViewById(R.id.image);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        image.setVisibility(View.GONE);
+    }
+}
